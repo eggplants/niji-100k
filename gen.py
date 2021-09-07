2ac40fc13eaa90ceab3ac02c9c10732480ae96ae
eggplants <w10776e8w@yahoo.co.jp>
2021-09-08 04:56:11 +0900

    fix

--- gen.py
+++ gen.py
@@ -1,25 +1,53 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import pandas as pd
+# In[2]:
 
+
+get_ipython().system('pip install -r requirements.txt -q')
+import pandas as pd
+from urllib.parse import quote
 pd.set_option('display.max_rows', None)
 
-url = 'https://wikiwiki.jp/nijisanji/'\
-      '%E3%83%A1%E3%83%B3%E3%83%90%E3%83%BC%E3%83%87%E3%83%BC%E3%82%BF'\
-      '%E4%B8%80%E8%A6%A7'
 
-df = pd.read_html(url)[-5]
+# In[3]:
+
+
+df = pd.read_html('https://wikiwiki.jp/nijisanji/'
+                  + quote('メンバーデータ一覧'))[-5]
 df.columns = [_[1] for _ in df.columns.to_flat_index()]
 
-df = df[df['10万人']!='-'].sort_values('10万人').loc[:,('チャンネル','10万人')]
-df_status = pd.read_csv('https://raw.githubusercontent.com/'
-                        'eggplants/nijisanji-v23d-status/'
-                        'master/result.csv'
-                       ).rename(columns={'name': 'チャンネル'})
+
+# In[4]:
+
+
+df = df[df['10万人']!='-']
+df = df.sort_values('10万人')
+df = df.loc[:,('チャンネル','10万人')]
+
+
+# In[5]:
+
+
+df_status = pd.read_csv(
+    'https://raw.githubusercontent.com/'
+    'eggplants/nijisanji-v23d-status/'
+    'master/result.csv'
+).rename(columns={'name': 'チャンネル'})
+
+
+# In[6]:
+
+
 df = df.merge(df_status)
 df = df.reset_index(drop=True)
 df.index.name = 'index'
 df.index += 1
+df
+
+
+# In[7]:
+
+
 df.to_csv('res.csv')
 
