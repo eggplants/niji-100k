6cdf2797de92b6883193e05464b5c7367b393762
eggplants <w10776e8w@yahoo.co.jp>
2023-02-19 07:22:04 +0900

    fix

--- gen.py
+++ gen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[0]
 
 
 import pandas as pd
@@ -9,33 +9,45 @@ from urllib.parse import quote
 pd.set_option('display.max_rows', None)
 
 
-# In[2]:
+# In[1]
 
 
-df = pd.read_html('https://wikiwiki.jp/nijisanji/'
-                  + quote('メンバーデータ一覧'))[-5]
-df.columns = [_[1] for _ in df.columns.to_flat_index()]
+df = pd.read_html(
+    'https://wikiwiki.jp/nijisanji/?cmd=popout' +
+    '&page=' + quote('メンバーデータ一覧') + '%2F' + quote('チャンネル登録者数') +
+    '&id=' + quote('大台突破日'))[0]
+df
 
 
-# In[3]:
+# In[2]
+
+
+df.columns = [s for p,s in df.columns]
+df = df.dropna(how='any')
+df
+
+
+# In[3]
 
 
 df = df[df['10万人']!='-']
 df = df.sort_values('10万人')
-df = df.loc[:,('チャンネル','10万人')]
+df = df.loc[:,('登録者数','10万人')]
+df
 
 
-# In[4]:
+# In[4]
 
 
 df_status = pd.read_csv(
     'https://raw.githubusercontent.com/'
     'eggplants/nijisanji-v23d-status/'
     'master/result.csv'
-).rename(columns={'name': 'チャンネル'})
+).rename(columns={'name': '登録者数'})
+df_status
 
 
-# In[5]:
+# In[5]
 
 
 df = df.merge(df_status)
@@ -45,7 +57,7 @@ df.index += 1
 df
 
 
-# In[6]:
+# In[6]
 
 
 df.to_csv('res.csv')
--- nb2src.sh
+++ nb2src.sh
@@ -2,3 +2,6 @@
 
 jupyter nbconvert --to script gen.ipynb
 cat <<<"$(sed '/get_ipython()\.system(/d' gen.py)" >gen.py
+
+awk '$0 == "# In[ ]:"{print"# In["a++"]"}$0 != "# In[ ]:"{print$0}' gen.py > _
+mv _ gen.py
